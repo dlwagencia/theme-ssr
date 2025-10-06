ad2bfbe13b2dd8c9791f91c546dfd1df57e4077c
Diego L <diegoleismann@gmail.com>
2025-10-06 18:40:46 -0300

    removido notFound.js

--- views/notFound.js
+++
@@ -1,8 +0,0 @@
-const Index = require('../index.js')
-const notFoundView = async (req, res) => {
-    const content = '<div id="not-found"><p>Página não encontrada</p></div>';
-    const title = "Página não encontrada";
-    const index = await Index.html(title, content);
-    res.send(index);
-}
-module.exports = notFoundView
